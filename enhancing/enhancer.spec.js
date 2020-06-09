85d1d66b999118e7b70f25aeb5448d76a1d8ef3c
Khoa Tran <jacktran1799@gmaail.com>
2020-06-09 11:51:12 -0500

    finish testing the success and repair method

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,13 @@
 const enhancer = require('./enhancer.js');
-// test away!
+
+describe("text enhancer methods", ()=>{
+    it("repair method should return durability to 100", ()=>{
+        expect(enhancer.repair({name:'Angel Sword',enhancement:3,durability:0})).toStrictEqual({name:'Angel Sword',enhancement:3,durability:100})
+    });
+    it('successfully enhance the item', ()=>{
+        expect(enhancer.succeed({name:'Angel Sword',enhancement:3,durability:0})).toStrictEqual({name:'Angel Sword',enhancement:4,durability:0})
+    })
+    it('keep the enhancement the same if it is 20', ()=>{
+        expect(enhancer.succeed({name:'Angel Sword',enhancement:20,durability:0})).toStrictEqual({name:'Angel Sword',enhancement:20,durability:0})
+    })
+});
